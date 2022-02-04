d4033e5b855530c7e5de87f0be19626cc94b9861
xm3karen <55311079+karenluuuu3@users.noreply.github.com>
2022-02-04 11:14:11 +0800

    220204
    check

--- js/index.js
+++ js/index.js
@@ -36,12 +36,12 @@ $(document).ready(function(){
 
 // fade to another page
 window.transitionToPage = function(href) {
-    document.querySelector('section').style.opacity = 0
+    document.querySelector('body').style.opacity = 0
     setTimeout(function() { 
         window.location.href = href
     }, 500)
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('section').style.opacity = 1
+    document.querySelector('body').style.opacity = 1
 })
