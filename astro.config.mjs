ef58c7694e797bb905569fa7cb4fb85f050482a9
Yuan Shih <85390677+Yuan0100@users.noreply.github.com>
2025-06-13 12:01:33 +0800

    feat: add fonts (with Astro's experimental fonts API)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Noto Sans TC",
+      cssVariable: "--font-noto-sans-tc",
+    }, {
+      provider: fontProviders.google(),
+      name: "Geist",
+      cssVariable: "--font-geist-sans"
+    }, {
+      provider: fontProviders.google(),
+      name: "Geist Mono",
+      cssVariable: "--font-geist-mono",
+    }]
+  }
 });
