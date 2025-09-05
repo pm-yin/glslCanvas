80eb451f8d9378330c47cae8ab1ab0a7ecfd4723
Yuan Shih <85390677+Yuan0100@users.noreply.github.com>
2025-09-06 00:22:36 +0800

    chore: add mdx feature to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import react from '@astrojs/react';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Yuan0100.github.io",
   base: "cmh-glslcanvas",
-  integrations: [react()],
+  integrations: [react(), mdx()],
   experimental: {
     fonts: [{
       provider: fontProviders.google(),
